e88964905e84a75544fbc6a8ec3066895215b23f
Alec Walsh <code@alecwalsh.name>
2025-01-22 19:33:04 -0500

    Make into shared lib, add some type definitions

--- include/chess_cpp/chess_cpp.hpp
+++ include/chess_cpp/chess_cpp.hpp
@@ -1,3 +1,24 @@
 #pragma once
 
 #include "chess_cpp/version.hpp"
+
+#include <cstddef>
+#include <cstdint>
+
+struct board_position {
+    std::uint8_t rank;
+    std::uint8_t file;
+};
+
+struct chess_move {
+    board_position start_position;
+
+    board_position target_position;
+};
+
+struct board_state {
+    int i = 4567;
+};
+
+extern "C" chess_move* get_valid_moves(board_state board_state, std::size_t* num_moves);
+extern "C" void free_moves(chess_move* moves);
--- src/chess_cpp.cpp
+++ src/chess_cpp.cpp
@@ -1,1 +1,24 @@
 #include "chess_cpp.hpp"
+
+#include <cstdlib>
+
+chess_move* get_valid_moves(board_state board_state, std::size_t* num_moves) {
+    std::size_t size = 10;
+
+    auto* result = new chess_move[size];
+
+    for(std::size_t i = 0; i < size; i++) {
+        result[i] = {
+            .start_position = {.rank = static_cast<std::uint8_t>(i), .file = 213},
+            .target_position = {.rank = static_cast<std::uint8_t>(board_state.i), .file = 2}
+        };
+    }
+
+    *num_moves = size;
+
+    return result;
+}
+
+void free_moves(chess_move* moves) {
+    delete[] moves;
+}
