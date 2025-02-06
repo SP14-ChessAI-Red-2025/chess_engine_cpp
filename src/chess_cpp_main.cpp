613d9022bed7c48296db8711b999e69fccb9a9a3
Alec Walsh <code@alecwalsh.name>
2025-02-06 07:21:27 -0500

    Add basic main function

--- src/chess_cpp_main.cpp
+++ src/chess_cpp_main.cpp
@@ -1,6 +1,18 @@
 #include <chess_cpp/chess_cpp.hpp>
 
+#include <iostream>
+#include <format>
+
 
 int main() {
+    auto board = chess::board_state::initial_board_state();
+
+    std::size_t num_moves = 0;
+
+    auto moves = chess::python::get_valid_moves(board, &num_moves);
+
+    std::cout << std::format("num_moves: {}", num_moves) << std::endl;
+
+    chess::python::free_moves(moves);
 
 }
